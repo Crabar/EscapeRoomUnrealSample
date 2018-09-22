e9c4698682280cc7b4451b90abd5cdf9b2dd3ed7
Crabar <crabar0@gmail.com>
2018-09-22 21:16:06 +0300

    .

---
+++ cmake-build-debug/CMakeFiles/EscapeRoom.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/_PROGRAMMING/EscapeRoom/Intermediate/Build/Win64/UE4Editor/Inc/BuildingEscape/BuildingEscape.init.gen.cpp" "D:/_PROGRAMMING/EscapeRoom/cmake-build-debug/CMakeFiles/EscapeRoom.dir/Intermediate/Build/Win64/UE4Editor/Inc/BuildingEscape/BuildingEscape.init.gen.cpp.obj"
+  "D:/_PROGRAMMING/EscapeRoom/Intermediate/Build/Win64/UE4Editor/Inc/BuildingEscape/PositionReport.gen.cpp" "D:/_PROGRAMMING/EscapeRoom/cmake-build-debug/CMakeFiles/EscapeRoom.dir/Intermediate/Build/Win64/UE4Editor/Inc/BuildingEscape/PositionReport.gen.cpp.obj"
+  "D:/_PROGRAMMING/EscapeRoom/Source/BuildingEscape/BuildingEscape.cpp" "D:/_PROGRAMMING/EscapeRoom/cmake-build-debug/CMakeFiles/EscapeRoom.dir/Source/BuildingEscape/BuildingEscape.cpp.obj"
+  "D:/_PROGRAMMING/EscapeRoom/Source/BuildingEscape/PositionReport.cpp" "D:/_PROGRAMMING/EscapeRoom/cmake-build-debug/CMakeFiles/EscapeRoom.dir/Source/BuildingEscape/PositionReport.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Intermediate/Build/Win64/UE4Editor/Development"
+  "../Intermediate/Build/Win64/UE4Editor/Development/BuildingEscape"
+  "../Intermediate/Build/Win64/UE4Editor/Inc/BuildingEscape"
+  "../Source/BuildingEscape"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/EscapeRoom.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EscapeRoom.dir/Intermediate/Build/Win64/UE4Editor/Inc/BuildingEscape/BuildingEscape.init.gen.cpp.obj"
+  "CMakeFiles/EscapeRoom.dir/Intermediate/Build/Win64/UE4Editor/Inc/BuildingEscape/PositionReport.gen.cpp.obj"
+  "CMakeFiles/EscapeRoom.dir/Source/BuildingEscape/BuildingEscape.cpp.obj"
+  "CMakeFiles/EscapeRoom.dir/Source/BuildingEscape/PositionReport.cpp.obj"
+  "EscapeRoom.pdb"
+  "EscapeRoom.exe"
+  "EscapeRoom.exe.manifest"
+  "EscapeRoom.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EscapeRoom.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
