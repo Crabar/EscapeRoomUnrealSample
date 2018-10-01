16195968bc6812660dcd277ac42aaaff0f192e11
Yaroslav Poliakov <ypoliakov@playtika.com>
2018-10-01 15:01:40 +0300

    added input

--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -23,6 +23,7 @@ void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
 	UE_LOG(LogTemp, Warning, TEXT("Grabber is reporting for duty!"));
+
 	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
 	if (PhysicsHandle)
 	{
@@ -30,10 +31,24 @@ void UGrabber::BeginPlay()
 	}
 	else
 	{
-		UE_LOG(LogTemp, Error, TEXT("No Physics Handle included in %s!", *GetOwner()->GetName()));
+		UE_LOG(LogTemp, Error, TEXT("No Physics Handle included in %s!"), *GetOwner()->GetName());
+	}
+
+	InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
+	if (InputComponent)
+	{
+		InputComponent->BindAction("Grab", IE_Pressed, this, &UGrabber::OnGrabPressed);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("No Input component included in %s!"), *GetOwner()->GetName());
 	}
 }
 
+void UGrabber::OnGrabPressed()
+{
+	UE_LOG(LogTemp, Warning, TEXT("GRAB!"));
+}
 
 // Called every frame
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
